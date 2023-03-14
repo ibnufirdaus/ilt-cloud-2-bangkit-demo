6a1be365aa0f45a5b32f5d614a972f29882790df
Ibnu Firdaus <ibnu.sofian@mekari.com>
2023-03-14 17:11:53 +0700

    complete solution

--- server-hapi/index.js
+++ server-hapi/index.js
@@ -1,5 +1,5 @@
-const Hapi = require('@hapi/hapi')
-const contacts = require('./contacts')
+const Hapi = require('@hapi/hapi');
+const contacts = require('./contacts');
 
 (async () => {
     const server = Hapi.server({
@@ -8,7 +8,69 @@ const contacts = require('./contacts')
     })
 
     server.route([
+        {
+            method: 'POST',
+            path: '/contacts',
+            handler: (request, h) => {
+                const {name, email, phone} = request.payload;
+                const id = contacts[contacts.length - 1].id + 1;
 
+                contacts.push(
+                    {id, name, email, phone}
+                );
+
+                const response = h.response({message: 'Contact is successfully created'});
+                response.code(201);
+                return response;
+            }
+        },
+        {
+            method: 'GET',
+            path: '/contacts',
+            handler: () => contacts,
+        },
+        {
+            method: 'DELETE',
+            path: '/contacts/{id}',
+            handler: (request, h) => {
+                const { id } = request.params;
+                const index = contacts.findIndex(
+                    contact => contact.id === parseInt(id)
+                );
+
+                if (index === -1) {
+                    const response = h.response({message: 'Contact not found!'});
+                    response.code(404);
+                    return response;
+                }
+
+                contacts.splice(index, 1);
+
+                return { message: 'Contact is successfully deleted'};
+            }
+        },
+        {
+            method: 'PUT',
+            path: '/contacts/{id}',
+            handler: (request, h) => {
+                const { id } = request.params;
+                const {name, email, phone} = request.payload;
+
+                const index = contacts.findIndex(
+                    contact => contact.id === parseInt(id)
+                );
+
+                if (index === -1) {
+                    const response = h.response({message: 'Contact not found!'});
+                    response.code(404);
+                    return response;
+                }
+
+                contacts[index] = {id, name, email, phone};
+
+                return { message: 'Contact is successfully updated'}
+            }
+        }
     ]);
 
     await server.start();
